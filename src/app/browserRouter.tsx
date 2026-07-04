dede1d28278e8b253aa5395c7909fdc1a0e4c6c5
Gatomek <tomasz.galinski@gmail.com>
2026-07-04 13:34:53 +0200

    Publishing to GitHub Pages - hotfix

--- src/app/browserRouter.tsx
+++ src/app/browserRouter.tsx
@@ -6,36 +6,39 @@ import {createBrowserRouter, Navigate} from 'react-router-dom';
 import {FileNotFoundView} from '../views/FileNotFoundView.tsx';
 import {appConfig, PageNotFoundProcessing} from '../AppConfig.ts';
 
-export const browserRouter = createBrowserRouter([
-    {
-        path: '',
-        element: <MainContent />,
-        children: [
-            {
-                path: '/',
-                element: <Navigate to="/aircrafts" replace />
-            },
-            {
-                path: '/aircrafts',
-                element: <AircraftsView />
-            },
-            {
-                path: '/settings',
-                element: <SettingsView />
-            },
-            {
-                path: '/profile',
-                element: <ProfileView />
-            },
-            {
-                path: '*',
-                element:
-                    appConfig.pageNotFoundProcessing === PageNotFoundProcessing.Info ? (
-                        <FileNotFoundView />
-                    ) : (
-                        <Navigate to="/" />
-                    )
-            }
-        ]
-    }
-]);
+export const browserRouter = createBrowserRouter(
+    [
+        {
+            path: '',
+            element: <MainContent />,
+            children: [
+                {
+                    path: '/',
+                    element: <Navigate to="/aircrafts" replace />
+                },
+                {
+                    path: '/aircrafts',
+                    element: <AircraftsView />
+                },
+                {
+                    path: '/settings',
+                    element: <SettingsView />
+                },
+                {
+                    path: '/profile',
+                    element: <ProfileView />
+                },
+                {
+                    path: '*',
+                    element:
+                        appConfig.pageNotFoundProcessing === PageNotFoundProcessing.Info ? (
+                            <FileNotFoundView />
+                        ) : (
+                            <Navigate to="/" />
+                        )
+                }
+            ]
+        }
+    ],
+    {basename: '/project-flightradar-frontend/'}
+);
